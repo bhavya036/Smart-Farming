5674e4d176a802d12dd5c7ddd92f3e63adf31e9f
Bhavya J Patel <85954191+bhavya036@users.noreply.github.com>
2022-11-20 12:31:51 +0530

    Update Data base Code.ino

--- Data base Code.ino
+++ Data base Code.ino
@@ -6,16 +6,10 @@ const int sensor_pin = A0;
 float sensorData = analogRead(A0);
 #define DHTTYPE DHT11
 DHT dht(DHTPIN,DHTTYPE);
-/****************************************
-* Define Constants
-****************************************/
-#define TOKEN "BBFF-juJ8AnLDWPD2d4OAz4FSybLcHFXUax" // Your Ubidots TOKEN
+#define TOKEN "BBFF-juJ8AnLDWPD2d4OAz4FSybLcHFXUax" // Ubidots Token
 #define WIFINAME "Bhavya's Narzo 20" //Your wifi SSID
 #define WIFIPASS "11111111" // Your Wifi Password
 Ubidots client(TOKEN);
-/****************************************
-* Auxiliar Functions
-****************************************/
 void callback(char* topic, byte* payload, unsigned int length) {
 Serial.print("Message arrived [");
 Serial.print(topic);
@@ -25,23 +19,19 @@ for (int i=0;i<length;i++) {
 }
 Serial.println();
 }
-/****************************************
-* Main Functions
-****************************************/
 void setup() {
 dht.begin();
-// put your setup code here, to run once:
-client.setDebug(true); //Pass a true or false bool value to activate debug messages
+client.setDebug(true); 
 Serial.begin(115200);
 client.wifiConnection(WIFINAME, WIFIPASS);
 client.begin(callback);
 }
 void loop() {
-// put your main code here, to run repeatedly:
+
 if(!client.connected()){
 client.reconnect();
 }
-// Publish temperature,humidity & Soil Moisture
+// temperature,humidity & Soil Moisture
 float Temperature = dht.readTemperature();
 float Humidity = dht.readHumidity(); 
   /*Serial.print("Soil Moisture(in Percentage) = ");
@@ -50,7 +40,7 @@ float Humidity = dht.readHumidity();
   delay(1000);
 client.add("temperature", Temperature);
 client.ubidotsPublish("temperature");
-client.add("humidity", Humidity); //Insert your variable Labels and the value to be sent
+client.add("humidity", Humidity); 
 client.ubidotsPublish("humidity");
 client.ubidotsPublish("temperature");
 float moisture_percentage;
